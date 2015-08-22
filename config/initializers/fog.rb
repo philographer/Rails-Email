02c33d4660e0df82582928caf061670cfa3387fd
Yoohogyun <yoohoogun114@naver.com>
2015-08-22 09:10:46 +0900

    Update fog.rb

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAJOJC5CTYELAWPZBQ',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     '',                        # required
+    aws_secret_access_key: '',                        uired
     region:                'ap-northeast-1',                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'bucket-dev-teemaero'                          # required
